33a8ebae19a5ab34925024d0780690dcc733585b
aman <amanget4@gmail.com>
2019-07-15 10:29:12 -0500

    Project Starter

---
+++ src/main/java/com/ea/ecommerceproductservicebackend/ECommerceProductServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ea.ecommerceproductservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProductServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceProductServiceBackendApplication.class, args);
+	}
+
+}
